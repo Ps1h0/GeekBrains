ef1fc0401ccd1f5bbc066af27051b7d78b474bfd
Ps1h0 <platonov.liceum@mail.ru>
2020-12-28 00:08:18 +0800

    6th practice

--- SixthPractice/src/Dog.java
+++ SixthPractice/src/Dog.java
@@ -1,31 +1,11 @@
 public class Dog extends Animal{
 
-    private final int MAX_RUN_DISTANCE = 500;
-    private final int MAX_SWIM_DISTANCE = 10;
-
     static int numOfDogs = 0;
     {
         numOfDogs++;
     }
 
     public Dog(String name) {
-        super(name);
+        super(name, 500, 10);
     }
-
-    @Override
-    public void run(int runDist) {
-        if(runDist < MAX_RUN_DISTANCE)
-            System.out.println(getName() + " пробежал " + runDist + " метров");
-        else
-            System.out.println(getName() + " не сможет пробежать " + runDist + " метров");
-    }
-
-    @Override
-    public void swim(int swimDist) {
-        if (swimDist < MAX_SWIM_DISTANCE)
-            System.out.println(getName() + " проплыл " + swimDist + " метров");
-        else
-            System.out.println(getName() + " не сможет проплыть " + swimDist + " метров");
-    }
-
 }
